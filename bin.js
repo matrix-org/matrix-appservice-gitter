2da6e97d9b6095284a84ab334b40e1310697d568
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2016-02-10 17:38:55 +0000

    Strip out IRC and Heroku logic from bin.js

--- bin.js
+++ bin.js
@@ -1,46 +1,14 @@
 #!/usr/bin/env node
 var gitterBot = require('./')
 
-function getIrcOpts () {
-  var ircOpts = process.env['GITTERBOT_IRC_OPTS']
-
-  if (ircOpts) {
-    try {
-      ircOpts = JSON.parse(process.env['GITTERBOT_IRC_OPTS'])
-    } catch (err) {
-      console.error('Invalid JSON in GITTERBOT_IRC_OPTS')
-      process.exit(1)
-    }
-  }
-
-  return ircOpts || {}
-}
-
 var opts = {
-  ircServer: process.env['GITTERBOT_IRC_SERVER'],
-  ircChannel: process.env['GITTERBOT_IRC_CHANNEL'],
-  ircNick: process.env['GITTERBOT_IRC_NICK'],
-  ircAdmin: process.env['GITTERBOT_IRC_ADMIN'],
-  ircOpts: getIrcOpts(),
   gitterApiKey: process.env['GITTERBOT_APIKEY'],
   gitterRoom: process.env['GITTERBOT_GITTER_ROOM']
 }
 
-if (!((opts.ircChannel || opts.ircOpts.channels) &&
-  opts.gitterApiKey && opts.gitterRoom && opts.ircNick)) {
+if (!(opts.gitterApiKey && opts.gitterRoom)) {
   console.error('You need to set the config env variables (see readme.md)')
   process.exit(1)
-}
-
-var herokuURL = process.env.HEROKU_URL
-if (herokuURL) {
-  var request = require('request')
-  require('http').createServer(function (req, res) {
-    res.end('ping heroku\n')
-  }).listen(process.env.PORT)
-  setInterval(function () {
-    request(herokuURL).pipe(process.stdout)
-  }, 5 * 60 * 1000)
 }
 
 gitterBot(opts)
