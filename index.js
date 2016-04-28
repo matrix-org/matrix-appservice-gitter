c88ee98985b710b7a00bf68ed54b4eb94dfcc943
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2016-04-28 18:38:13 +0100

    Move MatrixGitterBridge out into its own .js file

--- index.js
+++ index.js
@@ -12,63 +12,7 @@ var MatrixRoom = require("matrix-appservice-bridge").MatrixRoom;
 var GitterRoom = require("matrix-appservice-bridge").RemoteRoom;
 
 var BridgedRoom = require("./lib/BridgedRoom");
-
-function MatrixGitterBridge(bridge, gitter, config) {
-  this._bridge = bridge;
-  this._gitter = gitter;
-  this._gitterUserId = null;
-
-  var rules = [];
-  if (config.name_mangling) {
-    for (var i = 0; i < config.name_mangling.length; i++) {
-      var rule = config.name_mangling[i];
-
-      rules.push({
-        pattern: new RegExp(rule.pattern),
-        template: rule.template
-      });
-    }
-  }
-  this._name_mangling_rules = rules;
-}
-
-MatrixGitterBridge.prototype.getGitterUserId = function() {
-  if (this._gitterUserId)
-    return Promise.resolve(this._gitterUserId);
-
-  return this._gitter.currentUser().then((u) => {
-    this._gitterUserId = u.id;
-    return u.id;
-  });
-};
-
-MatrixGitterBridge.prototype.mangleName = function(name) {
-  var rules = this._name_mangling_rules;
-
-  for (var i = 0; i < rules.length; i++) {
-    var rule = rules[i];
-    var matches = rule.pattern.exec(name);
-    if (!matches)
-      continue;
-
-    // TODO: more groups?
-    return rule.template.replace('$1', matches[1]);
-  }
-
-  return name;
-}
-
-MatrixGitterBridge.prototype.getIntentFromLocalpart = function(localpart) {
-  return this._bridge.getIntentFromLocalpart(localpart);
-};
-
-MatrixGitterBridge.prototype.getRemoteUser = function(id) {
-  return this._bridge.getUserStore().getRemoteUser(id);
-};
-
-MatrixGitterBridge.prototype.putRemoteUser = function(user) {
-  return this._bridge.getUserStore().setRemoteUser(user);
-};
+var MatrixGitterBridge = require("./lib/MatrixGitterBridge");
 
 function runBridge(port, config) {
   var gitter = new Gitter(config.gitter_api_key);
