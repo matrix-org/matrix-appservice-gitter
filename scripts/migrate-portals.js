e91092b67ec88d0072d1df3b9a6f1a951e052a2b
Will Hunt <willh@matrix.org>
2020-12-10 16:25:22 +0000

    target correct room

--- scripts/migrate-portals.js
+++ scripts/migrate-portals.js
@@ -32,6 +32,26 @@ rl.on('line', (line) => {
     portalRooms.push({gitterRoomId, oldMatrixRoomId, alias});
 });
 
+async function getNewRoomId(alias) {
+    try {
+        const targetRoomId = await client.resolveRoom(alias);
+        return { targetRoomId, joined: false };
+    } catch (ex) {
+        if (dryRun) {
+            return {
+                targetRoomId: '!noroomyet:gitter.im',
+                joined: false,
+            };
+        }
+        console.log("Joining new gitter room...");
+        await new Promise((r) => setTimeout(r, 5000)); // Joins cost more in rate limits
+        return {
+            targetRoomId: await client.joinRoom(alias),
+            joined: true,
+        };
+    }
+}
+
 rl.on('close', async () => {
     let index = 0;
     try {
@@ -41,21 +61,23 @@ rl.on('close', async () => {
     }
     console.log(`Starting from index ${index}`);
     for (const {gitterRoomId, oldMatrixRoomId, alias} of portalRooms.slice(index)) {
-        await new Promise((r) => setTimeout(r, 4000));
-        const targetRoomId = await client.joinRoom(alias);
+        await new Promise((r) => setTimeout(r, 3000));
+        const {targetRoomId, joined} = await getNewRoomId(alias);
         console.log(`${gitterRoomId} -> ${targetRoomId} (from: ${oldMatrixRoomId})`);
         if (!targetRoomId) {
             console.log(`No target room for ${gitterRoomId}!`);
         }
         if (!dryRun) {
+            console.log("Joining new room...");
+            await client.joinRoom(alias)
             console.log("Add tombstone...");
-            await client.sendStateEvent(gitterRoomId, 'm.room.tombstone', '', {
+            await client.sendStateEvent(oldMatrixRoomId, 'm.room.tombstone', '', {
                 body: `The matrix.org Gitter bridge has been discontinued. You can view this channel on the new bridge over at ${alias}`,
                 replacement_room: targetRoomId,
             });
             console.log("Sending power level update");
             // Disallow people from talking, should provide incentive to join the new room
-            await client.sendStateEvent(gitterRoomId, 'm.room.power_levels', '', {
+            await client.sendStateEvent(oldMatrixRoomId, 'm.room.power_levels', '', {
                 "ban": 50,
                 "events": {
                     "m.room.avatar": 50,
@@ -74,8 +96,10 @@ rl.on('close', async () => {
                 },
                 "users_default": 0            
             });
-            console.log("Parting joined room");
-            await client.leaveRoom(targetRoomId);
+            if (joined) {
+                console.log("Parting joined room");
+                await client.leaveRoom(targetRoomId);
+            }
         }
         await fs.promises.writeFile('checkpoint.txt', `${index}`, 'utf-8');
         index++;
